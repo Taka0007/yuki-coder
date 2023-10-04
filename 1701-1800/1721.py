59606d66db747b0cb5a134ed9608d8c3a5116ac2
Taka0007 <7takacode@gmail.com>
2023-10-04 17:41:38 +0900

    solve No.1721

---
+++ 1701-1800/1721.py
@@ -0,0 +1,10 @@
+# 問題リンク: https://yukicoder.me/problems/no/1721
+# 提出リンク: https://yukicoder.me/submissions/917801
+
+N = input()
+
+if ('4' in N) and ('6' in N):
+    ans = 'Beautiful'
+else:
+    ans = '...'
+print(ans)
