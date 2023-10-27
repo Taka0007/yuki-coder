064d6d9144d46597123d929ba08a191865292790
Taka0007 <7takacode@gmail.com>
2023-10-28 03:20:55 +0900

    solve No.2515 by Rust

---
+++ 2501-2600/2515-SimilarTriangles.rs
@@ -0,0 +1,29 @@
+use std::io;
+
+fn main(){
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let values: Vec<i32> = input
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let _x1 = values[0];
+    let y1 = values[1];
+    let _x2 = values[2];
+    let y2 = values[3];
+    let _x3 = values[4];
+
+    let ans: &str;
+
+    if y1==y2 {
+        ans = "Yes";
+    }
+
+    else{
+        ans = "No";
+    }
+
+    println!("{}", ans);
+}
