9e16dbb4f09c6e4878e86cb763828aa3c0a26198
Taka <7takacode@gmail.com>
2023-12-19 16:36:46 +0900

    solve No.1089

---
+++ 1001-1100/1089  three variable equation.py
@@ -0,0 +1,11 @@
+# Problem link : https://yukicoder.me/problems/no/1089
+# Submission link : https://yukicoder.me/submissions/939192
+
+# input
+N = int(input())
+
+# solve
+ans = ((N+2)*(N+1))//2
+
+# output
+print(ans)
