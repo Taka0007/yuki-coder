111d05b9b7efa2408e5b469c1384ddf8eef08941
Taka <7takacode@gmail.com>
2024-03-30 18:00:34 +0900

    solved No.350

---
+++ 201-300/350-d=vt.py
@@ -0,0 +1,11 @@
+# Priblem link : https://yukicoder.me/problems/no/350
+# Submission link : https://yukicoder.me/submissions/966086
+
+# import
+from decimal import Decimal
+# input
+v,t = map(Decimal,input().split())
+# calculate
+ans = v*t
+# output
+print(int(ans))
